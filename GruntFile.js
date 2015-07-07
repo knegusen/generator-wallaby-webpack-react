e564f52d872373838cca759636a91c46d4cd6cb3
Dennis <axelsson.dennis@gmail.com>
2015-07-07 21:14:10 +0200

    Add watcher for tests.

--- GruntFile.js
+++ GruntFile.js
@@ -1,17 +1,33 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
         mochaTest: {
             unit: {
                 src: ['app/src-test/**/*Spec.js']
+            },
+            continuous: {
+                src: ['app/src-test/**/*Spec.js'],
+                clearRequireCache: true
             }
         },
         jshint: {
             all: ['Gruntfile.js', 'app/**/*.js'],
             options: {
                 node: true
+            }
+        },
+        watch: {
+            js: {
+                options: {
+                    spawn: true,
+                    interrupt: true,
+                    debounceDelay: 250,
+                },
+                files: ['Gruntfile.js', 'src/*.js', 'test/*.js'],
+                tasks: ['mochaTest:continuous']
             }
         }
     });
