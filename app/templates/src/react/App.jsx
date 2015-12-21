c8952052257467b2290da9c29279dffc646e414e
knegusen <knegusen@gmail.com>
2015-12-21 10:55:07 +0100

    Add correct .eslintrc-config-file.

--- app/templates/src/react/App.jsx
+++ app/templates/src/react/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExampleComponent from './ExampleComponent'
+import ExampleComponent from './ExampleComponent';
 
 ReactDOM.render(<ExampleComponent>This is the example
     component</ExampleComponent>, document.getElementById('appContent'));
