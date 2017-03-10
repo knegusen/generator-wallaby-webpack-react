849278e1c7e7f417100626c869f32ae26ccd0268
Dennis Axelsson <dennis.axelsson@etraveli.com>
2017-03-10 16:01:20 +0100

    Adapt webpack config to webpack 2.

--- app/templates/webpack.config.prod.js
+++ app/templates/webpack.config.prod.js
@@ -3,41 +3,37 @@ var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: [
-        './src/App.jsx'
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        }),
-        new CopyWebpackPlugin([
-            { from: 'public' }
-        ])
-    ],
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            include: path.join(__dirname, 'src'),
-            query: {
-                presets: ['es2015', 'stage-0', 'react']
-            }
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+  devtool: 'source-map',
+  entry: [
+    './src/App.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+    new CopyWebpackPlugin([
+      { from: 'public' }
+    ])
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: path.join(__dirname, 'src'),
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
